67db9dbe362123d58c19e85439fcb1ebffb308df
SaraPetre <sara.petre@chasacademy.se>
2023-07-21 10:34:28 +0000

    Upload New File

---
+++ readme.py
@@ -0,0 +1,17 @@
+# ('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+# ('|                        WELCOME                                  |')
+# ('|         1. Create user & save user to Guest List & csv-file.    |')
+# ('|         2. Load saved users                                     |')
+# ('|         3. Delete user                                          |')
+# ('|         4. End program                                          |')
+# ('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+
+# The file aras.py contains a meny for creating a guest list containing
+# a name and a residence.
+# The guest will be automatic saved in a csv-file and you can also se the 
+# list printed in the terminal.
+# If you want to download the saved gust list you can also do this by choising 2. Load saved users.
+# If you want to delete a guest from the list you can select the choise "Delete user".
+# Finaly if you want to end the program there is a selection for that as well.
+# //Sara Petré
